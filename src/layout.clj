ac37f07b8cc07affb1e810c8ce7325bb01e1fcaf
Christopher Kauffman <kauffman@glu16.cs.umn.edu>
2011-05-26 21:26:19 -0500

    Work on trying to get the form of constraints right for solving the layout problem

--- src/layout.clj
+++ src/layout.clj
@@ -2,9 +2,7 @@
   (:require [swank.core])
   (:use [clojure.contrib.def]))
 
-(defn get-glyph [o] o)
-
-
+(defn get-glyph [o c] o)
 
 (defn add-heads
   "Add note heads "
@@ -29,6 +27,49 @@
                    {:glyph glyph
                     :ypos 5             ;May need to adjust on rest type
                     :xpos '(:parent xpos)})))))))
+
+;; (defmacro add-constraints [obj key & body]
+;;   `(with-meta ~obj (assoc (meta ~obj) ~key (assoc {} ~@body))))
+
+;; (defmacro add-to-layout [obj key & body]
+;;   `(let [lay# (:layout ~obj)
+;;          nlay# (assoc lay# ~key (assoc nil ~@body))]
+;;      (assoc ~obj :layout nlay#)))
+
+(defn add-to-layout [obj key vals]
+  (let [lay (:layout obj)
+        nlay (assoc lay key vals)]
+     (assoc obj :layout nlay)))
+
+(defn add-heads
+  "Add note heads "
+  [context notes]
+  (if-let [pits (seq (pitches notes))]     ;Have pitches
+    (let [fpit (first pits)
+          rpit (next pits)
+          glyph (get-glyph (dur notes) context)
+          box (bbox glyph)              ;Bounding box for glyph
+          top (:topline context)      ;Pitch at top line
+          fx (gensym) fy (gensym)
+          nfpit (add-to-layout fpit :head
+                               {:x fx :y fy :glyph glyph
+                                :constr `(( 1 ~y ~(diff top fpit)))})
+          nrpit (map #(let [x (gensym) y (gensym)]
+                        (add-to-layout % :head
+                                       {:x x :y y :glyph glyph
+                                        :constr `((~(diff top %) 1 ~y )
+                                                  (0 1 ~fx -1 ~x))})) ;Move down to note
+                     rpit)]
+      (assoc notes :pitches (conj nrpit nfpit)))
+    ;; Have a rest
+    (let [glyph (get-glyph (keyword (str (dur notes) "rest")))
+            top (:topline context)]        ;Pitch at top line
+        (assoc notes :pitches
+               (with-meta pits
+                 (assoc (meta pits) :head
+                   {:glyph glyph
+                    :ypos 5             ;May need to adjust on rest type
+                    :xpos '(:parent xpos)}))))))
 
 (defvar dot-dist 1
   "Preferred distance from edge of bounding box of note heads")
